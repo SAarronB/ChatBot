0f3f2f64a07470f0a1e9926f62a506f0844483a1
SAaronB <programmingsean@gmail.com>
2019-04-23 09:52:47 -0600

    Added Twitter Methods

--- src/chat/model/chatTwitter.java
+++ src/chat/model/chatTwitter.java
@@ -1,8 +1,12 @@
 package chat.model;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 import chat.controller.ChatController;
 public class chatTwitter{
+	private Twitter chatTwitter;
 	private ChatController app;
 	public chatTwitter(ChatController app) {
 		this.app = app;
+		this.chatTwitter = TwitterFactory.getSingleton();
 	}
 }
